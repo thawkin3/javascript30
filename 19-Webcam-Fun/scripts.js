0dcfdaf28ff448d0625ca2b36c8564956cb56bf8
Tyler Hawkins <thawkins@instructure.com>
2019-06-20 16:44:33 -0600

    day 19: webcam fun

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,123 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const takePhotoButton = document.querySelector('#take-photo-button');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+// active filter
+let activeFilter = document.querySelector('[name="active-filter"]:checked').value;
+const filterOptions = {
+    'green-screen': greenScreen,
+    'rgb-split': rgbSplit,
+    'red-effect': redEffect,
+};
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+    
+//  DEPRECIATION : 
+//       The following has been depreceated by major browsers as of Chrome and Firefox.
+//       video.src = window.URL.createObjectURL(localMediaStream);
+//       Please refer to these:
+//       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+      
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    
+    // mess with them
+    pixels = filterOptions[activeFilter](pixels);
+    
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  // play the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // BLUE
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  console.log(levels);
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+function updateActiveFilter(e) {
+    activeFilter = e.target.value;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
+takePhotoButton.addEventListener('click', takePhoto);
+radioButtons.forEach(radioButton => radioButton.addEventListener('change', updateActiveFilter));
--- README.md
+++ README.md
@@ -77,3 +77,7 @@ My solutions for the JavaScript 30 Day Challenge.
 18. [Adding Up Times with Reduce](http://tylerhawkins.info/javascript30/18-Adding-Up-Times-with-Reduce/)
 
     Takeaways: `NodeLists` are not the same as `Arrays` and have different methods on their prototype. You can easily create an array from a `NodeList` using the `Array.from` method. `reduce` is a handy method to loop through your array and transform it into another data type, like an object or a number. When dealing with large data sets, you want to avoid looping over the data multiple times, as that is not very efficient.
+
+19. [Webcam Fun](http://tylerhawkins.info/javascript30/19-Webcam-Fun/)
+
+    Takeaways: Creating filters is fun! Canvases and videos can be used to display live video streams as well to take photos. Image data on a canvas can be retrieved, manipulated, and returned to create some neat effects.
