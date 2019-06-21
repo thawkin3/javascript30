0589dd0eb4e8ce7f5f1ad228eaecafe14e15d121
Tyler Hawkins <thawkins@instructure.com>
2019-06-21 13:30:58 -0600

    day 19: added more filter options

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -1,125 +1,177 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
-const takePhotoButton = document.querySelector('#take-photo-button');
-const radioButtons = document.querySelectorAll('input[type="radio"]');
+(function() {  
+  const video = document.querySelector('.player');
+  const canvas = document.querySelector('.photo');
+  const ctx = canvas.getContext('2d');
+  const strip = document.querySelector('.strip');
+  const snap = document.querySelector('.snap');
+  const takePhotoButton = document.querySelector('#take-photo-button');
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  const nonVideoContainer = document.querySelector('.non-video-container');
 
-// active filter
-let activeFilter = document.querySelector('[name="active-filter"]:checked').value;
-const filterOptions = {
+  let activeFilter = document.querySelector('[name="active-filter"]:checked').value;
+  const filterOptions = {
+    normal,
     'green-screen': greenScreen,
     'rgb-split': rgbSplit,
     'red-effect': redEffect,
-};
+    'grey-scale': greyScale,
+    embossed,
+    inverted,
+  };
 
-function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
-      console.log(localMediaStream);
-    
-//  DEPRECIATION : 
-//       The following has been depreceated by major browsers as of Chrome and Firefox.
-//       video.src = window.URL.createObjectURL(localMediaStream);
-//       Please refer to these:
-//       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+  function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then(localMediaStream => {
       
-      video.srcObject = localMediaStream;
-      video.play();
-    })
-    .catch(err => {
-      console.error(`OH NO!!!`, err);
-    });
-}
-
-function paintToCanvas() {
-  const width = video.videoWidth;
-  const height = video.videoHeight;
-  canvas.width = width;
-  canvas.height = height;
-
-  return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
-    let pixels = ctx.getImageData(0, 0, width, height);
-    
-    // mess with them
-    pixels = filterOptions[activeFilter](pixels);
-    
-    // put them back
-    ctx.putImageData(pixels, 0, 0);
-  }, 16);
-}
-
-function takePhoto() {
-  // play the sound
-  snap.currentTime = 0;
-  snap.play();
-
-  // take the data out of the canvas
-  const data = canvas.toDataURL('image/jpeg');
-  const link = document.createElement('a');
-  link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome" />`;
-  strip.insertBefore(link, strip.firstChild);
-}
-
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+    //  DEPRECIATION : 
+    //       The following has been depreceated by major browsers as of Chrome and Firefox.
+    //       video.src = window.URL.createObjectURL(localMediaStream);
+    //       Please refer to these:
+    //       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+    //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+        
+        video.srcObject = localMediaStream;
+        video.play();
+        setTimeout(resizeCanvas, 1000); // resize hack
+      })
+      .catch(err => {
+        console.error(`OH NO!!!`, err);
+      });
   }
-  return pixels;
-}
 
-function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // BLUE
+  function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height); // put the video on the canvas
+      let pixels = ctx.getImageData(0, 0, width, height); // take the pixels out
+      pixels = filterOptions[activeFilter](pixels); // mess with them
+      ctx.putImageData(pixels, 0, 0); // put them back
+    }, 16);
   }
-  return pixels;
-}
 
-function greenScreen(pixels) {
-  const levels = {};
+  function takePhoto() {
+    // play the sound
+    snap.currentTime = 0;
+    snap.play();
 
-  document.querySelectorAll('.rgb input').forEach((input) => {
-    levels[input.name] = input.value;
-  });
+    // take the data out of the canvas
+    // and create a downloadable image
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome" />`;
+    strip.insertBefore(link, strip.firstChild);
+  }
 
-  console.log(levels);
+  function normal(pixels) {
+    return pixels;
+  }
 
-  for (i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
-
-    if (red >= levels.rmin
-      && green >= levels.gmin
-      && blue >= levels.bmin
-      && red <= levels.rmax
-      && green <= levels.gmax
-      && blue <= levels.bmax) {
-      // take it out!
-      pixels.data[i + 3] = 0;
+  function redEffect(pixels) {
+    const pixelsLength = pixels.data.length;
+    for (let i = 0; i < pixelsLength; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
     }
+    return pixels;
   }
 
-  return pixels;
-}
+  function rgbSplit(pixels) {
+    const pixelsLength = pixels.data.length;
+    for (let i = 0; i < pixelsLength; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0]; // RED
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // BLUE
+    }
+    return pixels;
+  }
 
-function updateActiveFilter(e) {
+  function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+
+    const pixelsLength = pixels.data.length;
+    for (i = 0; i < pixelsLength; i = i + 4) {
+      const red = pixels.data[i + 0];
+      const green = pixels.data[i + 1];
+      const blue = pixels.data[i + 2];
+      const alpha = pixels.data[i + 3];
+
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+
+    return pixels;
+  }
+
+  function greyScale(pixels) {
+    const pixelsLength = pixels.data.length;
+    for (let i = 0; i < pixelsLength; i+=4) {
+      const r = pixels.data[i + 0]; // RED
+      const g = pixels.data[i + 1]; // GREEN
+      const b = pixels.data[i + 2]; // BLUE
+      const brightness = (3 * r + 4 * g + b)>>>3;
+      pixels.data[i] = brightness;
+      pixels.data[i+1] = brightness;
+      pixels.data[i+2] = brightness;
+    }
+    return pixels;
+  }
+
+  function embossed(pixels) {
+    const width = pixels.width;
+    const pixelsLength = pixels.data.length;
+    // Loop through the subpixels, convoluting each using an edge-detection matrix.
+    for (let i = 0; i < pixelsLength; i++) {
+      if (i % 4 === 3) { continue; }
+      pixels.data[i] = 127 + 2 * pixels.data[i] - pixels.data[i + 4] - pixels.data[i + width * 4];
+    }
+    return pixels;
+  }
+
+  function inverted(pixels) {
+    const pixelsLength = pixels.data.length;
+    for (let i = 0; i < pixelsLength; i+=4) {
+      pixels.data[i + 0] = 255 - pixels.data[i + 0]; // RED
+      pixels.data[i + 1] = 255 - pixels.data[i + 1]; // GREEN
+      pixels.data[i + 2] = 255 - pixels.data[i + 2]; // BLUE
+    }
+    return pixels;
+  }
+
+  function updateActiveFilter(e) {
     activeFilter = e.target.value;
-}
+  }
 
-getVideo();
+  function resizeCanvas() {
+    const maxHeight = window.innerHeight - nonVideoContainer.offsetHeight - 200;
+    const maxWidth = window.innerWidth;
+    // canvas.style.height = `${maxHeight}px`;
+    canvas.style.height = `${Math.min(maxHeight, (maxWidth * (video.videoHeight / video.videoWidth)))}px`;
+  }
 
-video.addEventListener('canplay', paintToCanvas);
-takePhotoButton.addEventListener('click', takePhoto);
-radioButtons.forEach(radioButton => radioButton.addEventListener('change', updateActiveFilter));
+  getVideo();
+  resizeCanvas();
+
+  video.addEventListener('canplay', paintToCanvas);
+  takePhotoButton.addEventListener('click', takePhoto);
+  radioButtons.forEach(radioButton => radioButton.addEventListener('change', updateActiveFilter));
+  window.addEventListener('resize', resizeCanvas);
+  window.addEventListener('orientationchange', resizeCanvas);
+})();
